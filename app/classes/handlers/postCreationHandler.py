99170331fd6a00f62bd597bae0d6725d280ba6e6
vh64g <vh64g@gmx.de>
2022-03-30 13:03:24 +0200

    Fixed: If file does not exist a default image will be selected to prevent the application from crashing

--- app/classes/handlers/postCreationHandler.py
+++ app/classes/handlers/postCreationHandler.py
@@ -1,3 +1,5 @@
+import os.path
+
 import app.data.basicData as bD
 from app.redirects.toClasses import create_post
 import app.classes.server_client_conn.send as send
@@ -13,6 +15,7 @@ class postCreationHandler:
         self.user = bD.user_name
         self.image_path = "data/test/img/Ag02.png"
         self.image = None
+        self.image_array = None
         self.content = ""
 
         self.post = None
@@ -29,8 +32,15 @@ class postCreationHandler:
 
     def create_post(self):
         # Image(source=self.image_path, size_hint=(1, None), size=(0, 300))
-        self.image = imageConverter.image2array(cv2.imread(self.image_path))
-        self.post = create_post(image=self.image, content=self.content, bvr=0)
+        try:
+            if os.path.isfile(self.image_path): self.image = cv2.imread(self.image_path)  # read image
+            else: self.image = cv2.imread("data/test/img/Ag02.png")  # default image
+        except:
+            self.image = cv2.imread("data/test/img/Ag02.png")  # default image
+
+        self.image_array = imageConverter.image2array(self.image)  # converts the image to a numpy array
+
+        self.post = create_post(image=self.image_array, content=self.content, bvr=0)  # creates the post
 
     def write(self):
         bD.new_posts.append(self.post)
