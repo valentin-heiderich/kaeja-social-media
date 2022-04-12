6d9620bd8de4d5c5ac0cd0077faa69d7e0d4f330
vh64g <vh64g@gmx.de>
2022-04-12 19:31:03 +0200

    App will not accept post without content

--- app/classes/handlers/postCreationHandler.py
+++ app/classes/handlers/postCreationHandler.py
@@ -18,10 +18,12 @@ class postCreationHandler:
         self.image = None
         self.image_array = None
         self.content = ""
+        self.valid_input = True
 
         self.post = None
 
         self.read()
+        self.check_input()
         self.create_post()
         # self.write()
         self.send()
@@ -31,7 +33,15 @@ class postCreationHandler:
         self.image_path = self.parent.children[1].ids.header_input.text  # gives the ability of a custom header
         self.content = self.parent.children[1].ids.content_input.text
 
+    def check_input(self):
+        if self.image_path == "":
+            pass
+        if self.content == "" or self.content == "Please enter some content":
+            self.valid_input = False
+            self.parent.children[1].ids.content_input.text = "Please enter some content"
+
     def create_post(self):
+        if not self.valid_input: return  # if the input is not valid, do not create a post
         # Image(source=self.image_path, size_hint=(1, None), size=(0, 300))
         try:
             if os.path.isfile(self.image_path): self.image = cv2.imread(self.image_path)  # read image
@@ -48,8 +58,10 @@ class postCreationHandler:
         pass
 
     def send(self):
+        if not self.valid_input: return  # if the input is not valid, do not send a post
         send.send(None, self.post)  # sends the post to the server
 
     def finish(self):
+        if not self.valid_input: return  # if the input is not valid, do not finish the post creation
         self.parent.children[1].ids.header_input.text = ""
         self.parent.children[1].ids.content_input.text = ""
